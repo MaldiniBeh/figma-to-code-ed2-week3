4158fca966ca243abb09bc96cfd3a7500b05e308
fredBoss <fredboss@kali>
2024-08-28 13:56:19 +0100

    build(component): sign-in component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+//const colors = require('tailwindcss/colors');
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // primary: colors.blue,
+        night: {
+          50: '#e4e4eb',
+          100: '#bbbace',
+          200: '#8f8ead',
+          300: '#66658c',
+          400: '#4b4777',
+          500: '#302a62',
+          600: '#2b245b',
+          700: '#241c51',
+          800: '#1c1445',
+          900: '#130030',
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
