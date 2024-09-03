8135133073d760472e31bc96d0f8df6ee3556e38
fredBoss <fredboss@kali>
2024-09-03 23:39:56 +0100

    build(component): sidebar & navbar component

--- src/app/components/layout/components/sidebar/sidebar.component.ts
+++ src/app/components/layout/components/sidebar/sidebar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [SidebarMenuComponent, AngularSvgIconModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
-
-}
+export class SidebarComponent {}
